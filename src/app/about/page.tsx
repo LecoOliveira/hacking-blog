cd6d2b45fab31f460b139a9d2c79c7b08af84b80
alex <lecoverde10@gmail.com>
2024-12-24 19:58:35 -0300

    style: Implementando responsividade da página Sobre/About

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,18 +5,23 @@ import FormContact from '@/components/formContact';
 export default function Page() {
   return (
     <div className="mb-48">
-      <div className="h-screen">
+      <div className="">
         <section
-          className="grid grid-cols-2 w-[900px] h-[500px] mx-auto mt-40 
-          bg-white/5 rounded-2xl rounded-l-full items-center"
+          className="flex flex-col sm:grid grid-cols-2 w-[280px] sm:w-[550px] 
+          md:w-[700px] lg:w-[900px] h-[460px] sm:h-[320px] md:h-[400px] 
+          lg:h-[500px] mx-auto relative sm:right-9 mt-12 sm:mt-40 bg-white/5 
+          rounded-2xl rounded-t-full sm:rounded-r-none sm:rounded-l-full 
+          items-center"
         >
           <article
-            className="m-8 w-[500px] h-[300px] text-center 
+            className="mt-16 sm:m-8 w-[220px] sm:w-[330px] md:w-[400px] 
+            lg:w-[500px] sm:h-[200px] md:h-[250px] lg:h-[300px] text-center 
             animate-fadeIn"
           >
-            <h1 className="text-6xl font-semibold mb-4"> Olá! </h1>
+            <h1 className="text-4xl lg:text-6xl font-semibold mb-4">Olá!</h1>
             <p
-              className="text-pretty font-extralight w-96 justify-center 
+              className="text-xs sm:text-xs md:text-sm lg:text-base 
+              font-extralight w-full sm:w-72 md:w-80 lg:w-96 justify-center 
               mx-auto"
             >
               E aí! Sou Alex Rocha, apaixonado por hacking e cyber security e
@@ -28,8 +33,9 @@ export default function Page() {
             </p>
           </article>
           <Image
-            className="m-8 ml-24 w-[430px] h-[430px] rounded-full 
-            animate-slideLeft"
+            className="p-12 sm:p-0 sm:m-8 w-100px h-100px sm:ml-24 lg:w-[430px] 
+            lg:h-[430px] rounded-full animate-slideUp
+            sm:animate-slideLeft"
             quality={100}
             src={image}
             alt="Imagem de perfil"
--- src/components/formContact.tsx
+++ src/components/formContact.tsx
@@ -6,34 +6,39 @@ export default function FormContact() {
       <div
         className="h-[1px] w-[200px] md:w-[400px] lg:w-[800px] self-stretch 
         bg-gradient-to-tr from-transparent via-white to-transparent 
-        opacity-25 mx-auto -mt-60 mb-24"
+        opacity-25 mx-auto mt-32 mb-24"
       />
       <form
         action={sendEmail}
-        className="flex flex-col items-center justify-center w-[800px]
+        className="flex flex-col items-center justify-center w-[340px] 
+        sm:w-[500px] md:w-[700px] lg:w-[800px]
         h-full m-auto"
       >
-        <h1 className="text-3xl font-semibold text-center mb-8">
+        <h1 className="text-2xl sm:text-3xl font-semibold text-center mb-8">
           Entre em contato
         </h1>
-        <p className="text-center w-[600px] font-thin mb-12 text-sm">
+        <p className="text-center w-full font-thin mb-12 text-xs sm:text-sm">
           Sinta-se à vontade para entrar em contato! Se você tiver uma pergunta,
           feedback ou uma proposta de colaboração, adoraria ouvir de você.
         </p>
-        <div className="flex flex-row w-full items-center justify-between">
+        <div
+          className="flex flex-col sm:flex-row w-full items-center 
+          justify-between"
+        >
           <input
             name="nome"
             type="text"
             placeholder="Nome"
-            className="w-[395px] h-[40px] border border-white/10 
-            rounded-md p-2 mb-4 bg-transparent focus:outline-none
-            placeholder:text-white/50 placeholder:text-sm"
+            className="w-full sm:w-[245px] md:w-[345px] lg:w-[395px] h-[40px] 
+            border border-white/10 rounded-md p-2 mb-4 bg-transparent 
+            focus:outline-none placeholder:text-white/50 placeholder:text-sm"
           />
           <input
             type="email"
             name="email"
             placeholder="Email"
-            className="w-[395px] h-[40px] border border-white/10
+            className="w-full sm:w-[245px] md:w-[345px] lg:w-[395px] 
+            h-[40px] border border-white/10
             rounded-md p-2 mb-4 bg-transparent focus:outline-none 
             placeholder:text-white/50 placeholder:text-sm"
           />
@@ -48,7 +53,8 @@ export default function FormContact() {
         />
         <button
           type="submit"
-          className="w-[300px] h-[40px] bg-[#d5d5d6] text-[#0e1018] 
+          className="w-[120px] sm:w-[200px] md:w-[300px] h-[40px] 
+          bg-[#d5d5d6] text-[#0e1018] 
           font-semibold hover:bg-opacity-70
           rounded-md mt-4"
         >
